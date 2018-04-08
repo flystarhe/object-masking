1354af9887d11b51ad03a8211f97a25e6bb1ba23
flystarhe <flystarhe@qq.com>
2018-04-08 11:36:10 +0800

    object_masking/model_mask_rcnn.py

--- object_masking/model_mask_rcnn.py
+++ object_masking/model_mask_rcnn.py
@@ -10,18 +10,16 @@ import pprint
 import numpy as np
 import skimage
 from keras.utils.data_utils import get_file
-# pip install imgaug
-from imgaug import augmenters as iaa
 # import mask_rcnn
-import utils
-import visualize
-import config as configlib
-import model as modellib
+import mrcnn.utils as mrcnn_utils
+import mrcnn.visualize as mrcnn_visualize
+import mrcnn.config as mrcnn_config
+import mrcnn.model as mrcnn_model
 # import object_masking
 from object_masking.utils_mask_rcnn import MyDataset
 
 
-class MyConfig(configlib.Config):
+class MyConfig(mrcnn_config.Config):
     def __init__(self, mydict, name="hej", gpu_count=1, images_per_gpu=1):
         # BATCH_SIZE = IMAGES_PER_GPU * GPU_COUNT
         # NUM_CLASSES = (1)Background + (?)NUM_CLASSES
@@ -41,7 +39,7 @@ def get_model(mode, config, model_dir, init_with="coco"):
     model_dir: Directory to save training logs and trained weights
     init_with: imagenet, coco, last, or model_path
     """
-    model = modellib.MaskRCNN(mode, config, model_dir)
+    model = mrcnn_model.MaskRCNN(mode, config, model_dir)
     if init_with == "imagenet":
         model_path = model.get_imagenet_weights()
         model.load_weights(model_path, by_name=True)
@@ -79,7 +77,7 @@ def dataset_display_masks(dataset, image_ids=None, limit=4):
         image = dataset.load_image(image_id)
         mask, class_ids = dataset.load_mask(image_id)
         print(dataset.image_reference(image_id))
-        visualize.display_top_masks(image, mask, class_ids, dataset.class_names, limit)
+        mrcnn_visualize.display_top_masks(image, mask, class_ids, dataset.class_names, limit)
 
 
 def dataset_display_boxes(dataset, image_ids=None):
@@ -90,9 +88,9 @@ def dataset_display_boxes(dataset, image_ids=None):
         image = dataset.load_image(image_id)
         mask, class_ids = dataset.load_mask(image_id)
         # Compute Bounding box
-        bbox = utils.extract_bboxes(mask)
+        bbox = mrcnn_utils.extract_bboxes(mask)
         print(dataset.image_reference(image_id))
-        visualize.display_instances(image, bbox, mask, class_ids, dataset.class_names)
+        mrcnn_visualize.display_instances(image, bbox, mask, class_ids, dataset.class_names)
 
 
 def dataset_display_instances(config, dataset, image_ids=None, augmentation=None):
@@ -101,14 +99,14 @@ def dataset_display_instances(config, dataset, image_ids=None, augmentation=None
         image_ids = dataset.image_ids
     for image_id in image_ids:
         image, image_meta, gt_class_id, gt_bbox, gt_mask =\
-            modellib.load_image_gt(dataset, config, image_id, augmentation=augmentation, use_mini_mask=False)
+            mrcnn_model.load_image_gt(dataset, config, image_id, augmentation=augmentation, use_mini_mask=False)
         print(dataset.image_reference(image_id))
-        modellib.log("image", image)
-        modellib.log("image_meta", image_meta)
-        modellib.log("gt_class_id", gt_class_id)
-        modellib.log("gt_bbox", gt_bbox)
-        modellib.log("gt_mask", gt_mask)
-        visualize.display_instances(image, gt_bbox, gt_mask, gt_class_id, dataset.class_names)
+        mrcnn_model.log("image", image)
+        mrcnn_model.log("image_meta", image_meta)
+        mrcnn_model.log("gt_class_id", gt_class_id)
+        mrcnn_model.log("gt_bbox", gt_bbox)
+        mrcnn_model.log("gt_mask", gt_mask)
+        mrcnn_visualize.display_instances(image, gt_bbox, gt_mask, gt_class_id, dataset.class_names)
 
 
 def training(model, dataset_train, dataset_val, stages=None):
@@ -156,7 +154,7 @@ def detect_display_masks(image, r, class_names, limit=4):
             image = skimage.color.gray2rgb(image)
         if image.shape[-1] == 4:
             image = image[..., :3]
-    visualize.display_top_masks(image, r["masks"], r["class_ids"], class_names, limit)
+    mrcnn_visualize.display_top_masks(image, r["masks"], r["class_ids"], class_names, limit)
 
 
 def detect_display_instances(image, r, class_names):
@@ -166,7 +164,7 @@ def detect_display_instances(image, r, class_names):
             image = skimage.color.gray2rgb(image)
         if image.shape[-1] == 4:
             image = image[..., :3]
-    visualize.display_instances(image, r["rois"], r["masks"], r["class_ids"], class_names, scores=r["scores"])
+    mrcnn_visualize.display_instances(image, r["rois"], r["masks"], r["class_ids"], class_names, scores=r["scores"])
 
 
 def evaluation(model, config, dataset, image_ids=None, verbose=0):
@@ -177,8 +175,8 @@ def evaluation(model, config, dataset, image_ids=None, verbose=0):
     for image_id in image_ids:
         # Load image and ground truth data
         image, image_meta, gt_class_id, gt_bbox, gt_mask =\
-            modellib.load_image_gt(dataset, config, image_id, use_mini_mask=False)
-        molded_images = np.expand_dims(modellib.mold_image(image, config), 0)
+            mrcnn_model.load_image_gt(dataset, config, image_id, use_mini_mask=False)
+        molded_images = np.expand_dims(mrcnn_model.mold_image(image, config), 0)
         # Run object detection
         results = model.detect([image], verbose)
         r = results[0]
@@ -186,17 +184,19 @@ def evaluation(model, config, dataset, image_ids=None, verbose=0):
         if r["class_ids"].size == 0:
             continue
         AP, precisions, recalls, overlaps =\
-            utils.compute_ap(gt_bbox, gt_class_id, gt_mask, r["rois"], r["class_ids"], r["scores"], r["masks"])
+            mrcnn_utils.compute_ap(gt_bbox, gt_class_id, gt_mask, r["rois"], r["class_ids"], r["scores"], r["masks"])
         APs.append(AP)
     return APs, np.mean(APs)
 
 
-def script_training():
-    classes = [("vgg_via", 1, "none")]
+def demo_script_training():
+    classes = [("vgg_via", 1, "ggo")]
     dataset_dir = "/data2/datasets/slyx/mjj_20180207/labeled_cd1/dataset00"
     dataset_val = get_dataset(dataset_dir, "", classes, "via_region_data_val.json")
     dataset_train = get_dataset(dataset_dir, "", classes, "via_region_data_train.json")
 
+    # pip install imgaug
+    from imgaug import augmenters as iaa
     augmentation = [iaa.Fliplr(0.5), iaa.Affine(scale=(0.8, 1.2), order=0), iaa.Affine(rotate=(-10, 10), order=0)]
     augmentation = iaa.SomeOf((0, None), augmentation)
     augmentation = iaa.Sometimes(0.5, augmentation)
